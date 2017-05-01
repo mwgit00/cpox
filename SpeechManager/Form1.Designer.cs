1e273c53d8b016ad52ec197939cead996d54c79b
Mark Whitney <mwgit00@earthlink.net>
2017-04-30 22:34:31 -0500

    new C# speech stuff
    Speech synthesis and recognition in C# app.

---
+++ SpeechManager/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace netsr2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUI = new System.Windows.Forms.TextBox();
+            this.audioLevelBar = new System.Windows.Forms.ProgressBar();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.checkBoxShowAudioProblems = new System.Windows.Forms.CheckBox();
+            this.checkBoxShowHypothesis = new System.Windows.Forms.CheckBox();
+            this.textBoxPhrase = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSpeak = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxEditPhrase = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUI
+            // 
+            this.textBoxUI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxUI.Location = new System.Drawing.Point(12, 130);
+            this.textBoxUI.Multiline = true;
+            this.textBoxUI.Name = "textBoxUI";
+            this.textBoxUI.ReadOnly = true;
+            this.textBoxUI.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxUI.Size = new System.Drawing.Size(555, 389);
+            this.textBoxUI.TabIndex = 0;
+            // 
+            // audioLevelBar
+            // 
+            this.audioLevelBar.Location = new System.Drawing.Point(83, 75);
+            this.audioLevelBar.Name = "audioLevelBar";
+            this.audioLevelBar.Size = new System.Drawing.Size(100, 22);
+            this.audioLevelBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.audioLevelBar.TabIndex = 1;
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(382, 12);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(90, 31);
+            this.buttonTest.TabIndex = 5;
+            this.buttonTest.Text = "Recognize";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // checkBoxShowAudioProblems
+            // 
+            this.checkBoxShowAudioProblems.AutoSize = true;
+            this.checkBoxShowAudioProblems.Location = new System.Drawing.Point(6, 21);
+            this.checkBoxShowAudioProblems.Name = "checkBoxShowAudioProblems";
+            this.checkBoxShowAudioProblems.Size = new System.Drawing.Size(167, 21);
+            this.checkBoxShowAudioProblems.TabIndex = 6;
+            this.checkBoxShowAudioProblems.Text = "Show Audio Problems";
+            this.checkBoxShowAudioProblems.UseVisualStyleBackColor = true;
+            this.checkBoxShowAudioProblems.CheckedChanged += new System.EventHandler(this.checkBoxShowAudioProblems_CheckedChanged);
+            // 
+            // checkBoxShowHypothesis
+            // 
+            this.checkBoxShowHypothesis.AutoSize = true;
+            this.checkBoxShowHypothesis.Location = new System.Drawing.Point(6, 48);
+            this.checkBoxShowHypothesis.Name = "checkBoxShowHypothesis";
+            this.checkBoxShowHypothesis.Size = new System.Drawing.Size(138, 21);
+            this.checkBoxShowHypothesis.TabIndex = 7;
+            this.checkBoxShowHypothesis.Text = "Show Hypothesis";
+            this.checkBoxShowHypothesis.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPhrase
+            // 
+            this.textBoxPhrase.Enabled = false;
+            this.textBoxPhrase.Location = new System.Drawing.Point(225, 87);
+            this.textBoxPhrase.Name = "textBoxPhrase";
+            this.textBoxPhrase.Size = new System.Drawing.Size(282, 22);
+            this.textBoxPhrase.TabIndex = 11;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(6, 75);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(71, 22);
+            this.textBoxStatus.TabIndex = 12;
+            this.textBoxStatus.Text = "Idle";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Location = new System.Drawing.Point(478, 12);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(90, 31);
+            this.buttonCancel.TabIndex = 13;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonSpeak
+            // 
+            this.buttonSpeak.Location = new System.Drawing.Point(285, 12);
+            this.buttonSpeak.Name = "buttonSpeak";
+            this.buttonSpeak.Size = new System.Drawing.Size(90, 31);
+            this.buttonSpeak.TabIndex = 14;
+            this.buttonSpeak.Text = "Speak";
+            this.buttonSpeak.UseVisualStyleBackColor = true;
+            this.buttonSpeak.Click += new System.EventHandler(this.buttonSpeak_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxShowAudioProblems);
+            this.groupBox1.Controls.Add(this.checkBoxShowHypothesis);
+            this.groupBox1.Controls.Add(this.textBoxStatus);
+            this.groupBox1.Controls.Add(this.audioLevelBar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 112);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recognition";
+            // 
+            // checkBoxEditPhrase
+            // 
+            this.checkBoxEditPhrase.AutoSize = true;
+            this.checkBoxEditPhrase.Location = new System.Drawing.Point(513, 88);
+            this.checkBoxEditPhrase.Name = "checkBoxEditPhrase";
+            this.checkBoxEditPhrase.Size = new System.Drawing.Size(54, 21);
+            this.checkBoxEditPhrase.TabIndex = 16;
+            this.checkBoxEditPhrase.Text = "Edit";
+            this.checkBoxEditPhrase.UseVisualStyleBackColor = true;
+            this.checkBoxEditPhrase.CheckedChanged += new System.EventHandler(this.checkBoxEditPhrase_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 531);
+            this.Controls.Add(this.checkBoxEditPhrase);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSpeak);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxPhrase);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.textBoxUI);
+            this.Name = "Form1";
+            this.Text = "Speech Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUI;
+        private System.Windows.Forms.ProgressBar audioLevelBar;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.CheckBox checkBoxShowAudioProblems;
+        private System.Windows.Forms.CheckBox checkBoxShowHypothesis;
+        private System.Windows.Forms.TextBox textBoxPhrase;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSpeak;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxEditPhrase;
+    }
+}
+
---
+++ SpeechManager/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace netsr2
+{
+    public partial class Form1 : Form
+    {
+        public SpeechRecognitionEngine recognizer;
+        public SpeechSynthesizer synth;
+        public String phrase = "this is a test";
+        public int word_ct = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set up all the speech synthesis stuff
+
+            synth = new SpeechSynthesizer();
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.SetOutputToDefaultAudioDevice();
+
+            synth.SpeakStarted +=
+                new EventHandler<SpeakStartedEventArgs>(
+                    tts_SpeakStarted);
+            synth.SpeakCompleted +=
+                new EventHandler<SpeakCompletedEventArgs>(
+                    tts_SpeakCompleted);
+
+            // set up all the recognition stuff
+
+            recognizer = new SpeechRecognitionEngine();
+            loadSinglePhraseGrammar(phrase);
+
+            recognizer.SpeechRecognized +=
+                new EventHandler<SpeechRecognizedEventArgs>(
+                    sre_SpeechRecognized);
+            recognizer.RecognizeCompleted +=
+                new EventHandler<RecognizeCompletedEventArgs>(
+                    sre_RecognizeCompletedHandler);
+            recognizer.SpeechRecognitionRejected +=
+                new EventHandler<SpeechRecognitionRejectedEventArgs>(
+                    sre_SpeechRecognitionRejectedHandler);
+            recognizer.SpeechHypothesized +=
+                new EventHandler<SpeechHypothesizedEventArgs>(
+                    sre_SpeechHypothesized);
+            recognizer.SpeechDetected +=
+                new EventHandler<SpeechDetectedEventArgs>(
+                    sre_SpeechDetected);
+
+            recognizer.AudioSignalProblemOccurred +=
+                new EventHandler<AudioSignalProblemOccurredEventArgs>(
+                    sre_AudioSignalProblemOccurred);
+            recognizer.AudioLevelUpdated +=
+                new EventHandler<AudioLevelUpdatedEventArgs>(
+                    sre_AudioLevelUpdated);
+            recognizer.AudioStateChanged +=
+                new EventHandler<AudioStateChangedEventArgs>(
+                    sre_AudioStateChanged);
+
+            // hope this magically picks whatever audio input is connected
+
+            recognizer.SetInputToDefaultAudioDevice();
+
+            // set up GUI
+
+            textBoxPhrase.Text = phrase;
+            textBoxUI.AppendText("Ready!" + Environment.NewLine);
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (checkBoxShowHypothesis.Checked == true)
+            {
+                textBoxUI.AppendText(Environment.NewLine);
+            }
+
+            textBoxUI.AppendText(e.Result.Text + Environment.NewLine);
+        }
+
+        void sre_AudioSignalProblemOccurred(object sender, AudioSignalProblemOccurredEventArgs e)
+        {
+            if (checkBoxShowAudioProblems.Checked == true)
+            {
+                // show all the gory details if desired
+
+                StringBuilder details = new StringBuilder();
+
+                details.AppendLine("Audio signal problem information:");
+                details.AppendFormat(
+                  " Audio level:               {0}" + Environment.NewLine +
+                  " Audio position:            {1}" + Environment.NewLine +
+                  " Audio signal problem:      {2}" + Environment.NewLine +
+                  " Recognizer audio position: {3}" + Environment.NewLine,
+                  e.AudioLevel,
+                  e.AudioPosition,
+                  e.AudioSignalProblem,
+                  e.RecognizerAudioPosition);
+
+                textBoxUI.AppendText(details.ToString());
+            }
+        }
+
+        void sre_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            // progress bar is a simple audio meter
+            audioLevelBar.Value = e.AudioLevel;
+        }
+
+        void sre_AudioStateChanged(object sender, AudioStateChangedEventArgs e)
+        {
+            AudioState newState = e.AudioState;
+            if (newState == AudioState.Silence)
+            {
+                textBoxStatus.BackColor = Color.Green;
+                textBoxStatus.Text = "Silence";
+            }
+            else if (newState == AudioState.Speech)
+            {
+                textBoxStatus.BackColor = Color.Yellow;
+                textBoxStatus.Text = "Speech";
+            }
+            else if (newState == AudioState.Stopped)
+            {
+                textBoxStatus.BackColor = Color.CadetBlue;
+                textBoxStatus.Text = "Stopped";
+                buttonTest.Enabled = true;
+                buttonCancel.Enabled = false;
+                buttonSpeak.Enabled = true;
+                audioLevelBar.Value = 0;
+            }
+        }
+
+        void sre_SpeechRecognitionRejectedHandler(
+          object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            textBoxUI.AppendText("rejected" + Environment.NewLine);
+#if FALSE
+            Console.WriteLine(" In SpeechRecognitionRejectedHandler:");
+
+            string grammarName = "<not available>";
+            string resultText = "<not available>";
+            if (e.Result != null)
+            {
+                if (e.Result.Grammar != null)
+                {
+                    grammarName = e.Result.Grammar.Name;
+                }
+                resultText = e.Result.Text;
+            }
+
+            Console.WriteLine(" - Grammar Name = {0}; Result Text = {1}",
+              grammarName, resultText);
+#endif
+        }
+
+        // Handle the RecognizeCompleted event.
+        void sre_RecognizeCompletedHandler(object sender, RecognizeCompletedEventArgs e)
+        {
+            textBoxUI.AppendText("completed" + Environment.NewLine);
+        }
+
+        void sre_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            if (checkBoxShowHypothesis.Checked == true)
+            {
+                for (int k = word_ct; k < e.Result.Words.Count; k++)
+                {
+                    String sk = e.Result.Words[k].Text;
+                    textBoxUI.AppendText(sk + "? ");
+                }
+                word_ct = e.Result.Words.Count;
+            }
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            textBoxUI.AppendText("Recognizing..." + Environment.NewLine);
+            word_ct = 0;
+            buttonTest.Enabled = false;
+            buttonCancel.Enabled = true;
+            buttonSpeak.Enabled = false;
+            textBoxStatus.BackColor = Color.Green;
+            textBoxStatus.Text = "Silence";
+            recognizer.RecognizeAsync();
+        }
+
+        private void checkBoxShowAudioProblems_CheckedChanged(object sender, EventArgs e)
+        {
+            //String s = checkBoxShowAudioProblems.Checked.ToString();
+            //textBoxUI.AppendText("Show Audio Problems: " + s + Environment.NewLine);
+        }
+
+        private void sre_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+#if FALSE
+            Console.WriteLine("  Speech detected at AudioPosition = {0}", e.AudioPosition);
+#endif
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            buttonCancel.Enabled = false;
+            recognizer.RecognizeAsyncCancel();
+            synth.SpeakAsyncCancelAll();
+        }
+
+        private void buttonSpeak_Click(object sender, EventArgs e)
+        {
+            synth.SpeakAsync(phrase);
+            buttonTest.Enabled = false;
+            buttonSpeak.Enabled = false;
+        }
+
+        void tts_SpeakStarted(object sender, SpeakStartedEventArgs e)
+        {
+            textBoxUI.AppendText("TTS started." + Environment.NewLine);
+        }
+
+        void tts_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            buttonTest.Enabled = true;
+            buttonSpeak.Enabled = true;
+            textBoxUI.AppendText("TTS completed." + Environment.NewLine);
+        }
+
+        private void checkBoxEditPhrase_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxPhrase.Enabled = checkBoxEditPhrase.Checked;
+            if (checkBoxEditPhrase.Checked == false)
+            {
+                // turning off editing of phrase
+                // will apply it as new grammar
+
+                recognizer.UnloadAllGrammars();
+                loadSinglePhraseGrammar(textBoxPhrase.Text);
+                textBoxUI.AppendText("New phrase applied." + Environment.NewLine);
+            }
+        }
+
+        private void loadSinglePhraseGrammar(String newPhrase)
+        {
+            phrase = newPhrase;
+            
+            // create a simple one-phrase grammar
+            Choices phraseChoice = new Choices();
+            phraseChoice.Add(new string[] {
+                newPhrase,
+            });
+
+            // create a GrammarBuilder and append the Choices object
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(phraseChoice);
+
+            // then create the Grammar instance and load it
+            // into the speech recognition engine.
+            Grammar g = new Grammar(gb);
+            recognizer.LoadGrammar(g);
+        }
+    }
+}
